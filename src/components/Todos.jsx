9dcda413d4a1d063d758fab35c5e45c482184bd6
ar-alamin <aralamin.pi@yahoo.com>
2023-05-09 21:34:07 +0600

    Todo

--- src/components/Todos.jsx
+++ src/components/Todos.jsx
@@ -4,25 +4,50 @@ import './Todos.css'
 function Todos() {
     const [inputData, setInputData] = useState('');
     const [items, setItems] = useState([]);
+    const [toggle, setToggle] = useState(true);
+    const [isEditItem, setIsEditItem] = useState(null);
 
     const addItems = ()=> {
         if(!inputData){
-
+            alert('plese fill data');
+        } else if(inputData && !toggle) {
+            setItems(
+                items.map((element)=> {
+                    if(element.id === isEditItem){
+                        return {...element, name: inputData}
+                    }
+                    return element;
+                })
+            )
+            setToggle(true);
+            setInputData('');
+            setIsEditItem(null);
+            
         } else {
-            setItems([...items, inputData]);
+            const allInputData = { id: new Date().getTime().toString(), name: inputData }
+            setItems([...items, allInputData]);
             setInputData('');
         }    
     }
 
-    const deleteItem = (id)=> {
-        const updateItems = items.filter((element, index)=> {
-            return index !== id;
+    const deleteItem = (index)=> {
+        const updateItems = items.filter((element)=> {
+            return index !== element.id;
         })
         setItems(updateItems);
     }
 
     const removeAll = ()=> {
         setItems([]);
+    }
+
+    const editItem = (id)=> {
+        let newEditItem = items.find((element)=>{
+            return element.id === id;
+        });
+        setToggle(false);
+        setInputData(newEditItem.name);
+        setIsEditItem(id);
     }
     
     return (
@@ -33,19 +58,22 @@ function Todos() {
 
             <div className="todo-list">
                 <div className="list-head">
-                    <input type="text" className='entered-list' placeholder='Add items...' value={inputData} onChange={(e)=> setInputData(e.target.value)} />
-                    <button className="add-list" onClick={addItems}>ADD</button>
+                    <input type="text" className='entered-list' placeholder='✍️Add items...' value={inputData} onChange={(e)=> setInputData(e.target.value)} />
+
+                    {
+                        toggle ? <button className="add-list" onClick={addItems}><i class="fa-solid fa-plus" title='Add Item' /></button> : <button className="add-list" onClick={addItems}><i class="fa-solid fa-pen-to-square" title='Edit Item' /></button>
+                    }
                 </div>
 
                 <div className="tasks">
                     {
-                        items.map((element, index)=> {
+                        items.map((element)=> {
                             return(
-                                <div className="item" key={index}>
-                                    <p>{element}</p>
+                                <div className="item" key={element.id}>
+                                    <p>{element.name}</p>
                                     <div className="item-btn">
-                                    <i class="fa-solid fa-pen-to-square" title="Edit Item"></i>
-                                    <i class="fa-solid fa-trash" title="Remove Item" onClick={()=> deleteItem(index)}></i>
+                                    <i class="fa-solid fa-pen-to-square" title="Edit Item" onClick={()=> editItem(element.id)}></i>
+                                    <i class="fa-solid fa-trash" title="Remove Item" onClick={()=> deleteItem(element.id)}></i>
                                 </div>
                             </div>
                             )
